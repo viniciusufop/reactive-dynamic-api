c35106afed5bcb0e569f50375286ca037c7b9f77
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-11-15 10:23:49 -0300

    commit inicial

---
+++ src/test/java/br/com/vfs/reactivedynamicapi/ReactiveDynamicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vfs.reactivedynamicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDynamicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
